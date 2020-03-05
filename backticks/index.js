e723cff523632d0cc48245cd1baea9ce6bfb7285
UrielGlez <ugj11@hotmail.com>
2020-03-05 10:54:36 -0700

    backticks y operador ternario

---
+++ backticks/index.js
@@ -0,0 +1,4 @@
+function saludar(nombre, sexo) {
+    var mensaje = (sexo == '1') ? `Bienvenido ${nombre}!` : `Bienvenida ${nombre}!`;
+    document.getElementById("mensaje").innerHTML = mensaje;
+}
