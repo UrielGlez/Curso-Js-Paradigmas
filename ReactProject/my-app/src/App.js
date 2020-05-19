bce3c10539cdbb3e354b96b05861af1b0fed425b
UrielGlez <ugj11@hotmail.com>
2020-05-19 17:25:38 -0600

    React Project

---
+++ ReactProject/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Evento from './components/Evento';
+
+
+function App() {
+  return (
+    <div>
+      <h1>EJERCICIO 20</h1>
+      <Evento/>
+    </div>
+  );
+}
+
+export default App;
